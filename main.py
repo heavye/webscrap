51b4181b82e6dba70e385c5ed41350b2630ec01e
heavye <euelfantaye@gmail.com>
2021-12-11 15:52:06 +0300

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+soup = BeautifulSoup(html_text, 'lxml')
